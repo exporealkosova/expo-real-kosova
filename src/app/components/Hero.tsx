4ef71ab1f89fff337ecf34518027aa5078230776
exporealkosova <exporealkosovaform@gmail.com>
2026-01-29 20:59:56 +0100

    Update Hero.tsx

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -2,7 +2,7 @@ const Hero = () => {
   return (
     <div className="flex justify-center items-center h-full md:max-h-[360px]">
       <video
-        src="https://res.cloudinary.com/dwinvxbiw/video/upload/v1769371075/EXPO_Real_AfterMovie_Compressed_01_tsilcg.mp4"
+        src="https://res.cloudinary.com/dwinvxbiw/video/upload/v1769716758/EXPO_Real_AfterMovie_1_mvt88s.mp4"
         className="w-full h-full object-cover md:h-[360px]"
         width={1920}
         height={1080}
@@ -15,4 +15,5 @@ const Hero = () => {
 };
 
 export default Hero;
+
 
