2f68c3b19e0a53e0bee2117bfdffb96d4c08364c
exporealkosova <exporealkosovaform@gmail.com>
2026-02-26 18:25:18 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,10 @@ const nextConfig: NextConfig = {
         // optional: restrict to your Cloudinary cloud name
         // pathname: "/dwinvxbiw/**",
       },
+      {
+        protocol: "https",
+        hostname: "a6wxlnk8hx.ufs.sh",
+      },
     ],
   },
 };
